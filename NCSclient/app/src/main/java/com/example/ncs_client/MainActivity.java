d1e034b67f88d099923c0ef55931eaca595ea2ca
michelepenzo <michelepenzo@outlook.it>
2019-11-16 16:09:28 +0100

    primo layout app

---
+++ NCSclient/app/src/main/java/com/example/ncs_client/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.ncs_client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
