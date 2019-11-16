a4457ad8c3ee5d0a1b74e0b3ffaa2563feea58a6
michelepenzo <michelepenzo@outlook.it>
2019-11-16 16:43:42 +0100

    permission

--- NCSclient/app/src/main/java/com/example/ncs_client/MainActivity.java
+++ NCSclient/app/src/main/java/com/example/ncs_client/MainActivity.java
@@ -1,12 +1,9 @@
+
 package com.example.ncs_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
