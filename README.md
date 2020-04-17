c3d598a9e2a8bd2a174a77c0bb89a7fd9bcbff78
michelepenzo <michelepenzo@outlook.it>
2020-04-17 16:04:12 +0200

    rename

--- README.md
+++ README.md
@@ -5,9 +5,11 @@ Esmpio di NCS, svolto come progetto per il corso NES. <br>
 ## Prerequisiti
 
 - Server:
-	- SO Linux (sviluppato e testato su Ubuntu 19.10) <br>
+	- SO Linux <br>
 	- python 3.7
-- Client: Smartphone Android <br>
+- Client:
+	- Smartphone Android _oppure_<br>
+	- PC con python 3.7
 - Dispositivi connessi alla stessa rete WiFi. <br>
 
 ## Funzionamento
@@ -22,13 +24,23 @@ cd elaborato-nes
 ./setup.sh
 ```
 
-
-3. Eseguire il __server__ guardando i valori dell'indirizzo IP e della porta.
+3. Eseguire il __server__ guardando il valore dell'indirizzo IP.
 ```
 python3 NCSserver/server.py
 ```
 
-4. Per il __client__ installare il file ``NCS-client.apk``, aprirlo e inserire i valori ottenuti dal server.
+4. Per il __client__:
+	- da *smartphone Android* installare il file ``client_app.apk`` che si trova in ``NCSclient_interface``, aprirlo e inserire l'indirizzo IP ottenuto dal server.
+	- da *PC* aprire un terminale ed eseguire:
+	```
+	cd NCSclient_interface
+	python3 client_pc.py
+	```
+
+5. Eseguire l'applicazione interattiva:
+```
+python3 circle_gui.py
+```
 
 
 ## Licenza
--- circle_gui.py
+++ circle_gui.py
@@ -16,13 +16,15 @@ class Paint(object):
         self.root=Tk()
         self.root.title('NCS Server')
         
-        
+        # get width and height
         _width=self.root.winfo_screenwidth()
         _height=self.root.winfo_screenheight()
 
+        # canvas
         self.c = Canvas(self.root, bg='white', width=_width, height=_height)
         self.c.grid(row=1, columnspan=5)
 
+        # distance labele
         self.label_error = Label(self.root, text='Distanza dal cerchio --->   ', font=("Helvetica", 16))
         self.label_error.grid(sticky = E, row=0, column=0) 
         
@@ -30,14 +32,15 @@ class Paint(object):
         self.label_error = Label(self.root, text='', font=("Helvetica", 16))
         self.label_error.grid(sticky = W, row=0, column=1) 
 
-        # TODO center oval con disegno con x1 e y1
         pos_y, pos_x =  _width/2, _height/2
-
-        self.c.create_oval(pos_x-500, pos_y-500, pos_x, pos_y, outline="#ff0000", fill="#1fba32", width=2)
+        #pos_x-500, pos_y-500, pos_x, pos_y
+        # TODO center oval con disegno con x1 e y1
+        self.c.create_oval(10,10,80,80, outline="#ff0000", fill="#1fba32", width=2)
         
         self.setup()
         
         self.root.mainloop()
+
 
     # set base values
     def setup(self):
@@ -50,6 +53,7 @@ class Paint(object):
         self.c.bind('<B1-Motion>', self.paint)
         self.c.bind('<ButtonRelease-1>', self.reset)
 
+
     # method for writing inside whiteboard
     def paint(self, event):
     
@@ -61,9 +65,9 @@ class Paint(object):
 
         # calculating distance from circle
         distance = 0
-
         
         self.label_error['text'] = distance
+
 
     # reset value
     def reset(self, event):
